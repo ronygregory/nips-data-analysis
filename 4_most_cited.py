adbde9d13a28b284881db16e9977d1dde161e85d
Rony Gregory <rony.gregory@utah.edu>
2016-04-06 20:38:34 -0600

    Added results to the file

--- 4_most_cited.py
+++ 4_most_cited.py
@@ -53,14 +53,12 @@ matching_set = set()
 count = 0
 ref_count_map = {}
 
-reference_list = reference_list[:10000] #TODO
 print len(reference_list)
 done_list = []
 kgram_map = {}
 for i, reference in enumerate(reference_list):
     kgram_map[i] = kgram(reference)
     
-print i 
 for i, reference in enumerate(reference_list):
     if i % 100 == 0:
         print i, " ", count
@@ -79,3 +77,15 @@ sorted_x = sorted(ref_count_map.items(), key=operator.itemgetter(1), reverse=Tru
 print sorted_x
 for val in sorted_x:
     print reference_list[val[0]],", ", val[1]
+
+# Top 10 cited papers:
+# A. Krizhevsky, I. Sutskever, and G. Hinton. Imagenet classification with deep convolutional neural networks. In NIPS, 2012. ,  14
+# K. Simonyan and A. Zisserman. Very deep convolutional networks for large-scale image recognition. In ICLR, 2015. ,  12
+# Y. LeCun, L. Bottou, Y. Bengio, and P. Haffner. Gradient-based learning applied to document recognition. 1998. ,  11
+# Y. Jia, E. Shelhamer, J. Donahue, S. Karayev, J. Long, R. Girshick, S. Guadarrama, and T. Darrell. Caffe: Convolutional architecture for fast feature embedding. arXiv:1408.5093, 2014. ,  10
+# S. Hochreiter and J. Schmidhuber. Long short-term memory. Neural computation, 9(8):17351780, 1997. ,  10
+# M. Lichman. UCI machine learning repository, 2013. ,  10
+# A. Krizhevsky, I. Sutskever, and G. E. Hinton. Imagenet classification with deep convolutional neural networks. In Advances in Neural Information Processing Systems 27, pages 10971105, 2012. ,  9
+# D. J. Rezende, S. Mohamed, and D. Wierstra. Stochastic backpropagation and approximate inference in deep generative models. In ICML, 2014. ,  8
+# John Duchi, Elad Hazan, and Yoram Singer. Adaptive subgradient methods for online learning and stochastic optimization. The Journal of Machine Learning Research, 12:21212159, 2011. ,  8
+# R. Girshick, J. Donahue, T. Darrell, and J. Malik. Rich feature hierarchies for accurate object detection and semantic segmentation. In CVPR, 2014. ,  7
